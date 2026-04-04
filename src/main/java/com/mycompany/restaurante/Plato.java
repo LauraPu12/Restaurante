888601c92c549c37c4f4af0833551b948033adb4
Olga Lucia Guacaneme Archila <est.olga.guacaneme@unimilitar.edu.co>
2026-04-04 17:30:09 -0500

    Se crearon los constructores de la clase plato

--- src/main/java/com/mycompany/restaurante/Plato.java
+++ src/main/java/com/mycompany/restaurante/Plato.java
@@ -2,5 +2,17 @@
 package com.mycompany.restaurante;
 
 public class Plato {
-    
+    String nombre; // Guarda el nombre del plato
+    double precio; // Guarda el precio del plato
+
+    // Constructor vacío
+    public Plato() {
+        // No inicializa valores
+    }
+
+    // Constructor con parámetros
+    public Plato(String nombre, double precio) {
+        this.nombre = nombre; // Asigna el nombre recibido
+        this.precio = precio; // Asigna el precio recibido
+    }
 }
