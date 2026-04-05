ddeff833fd7b1ca95248e38aebf388c3f2979978
Laura Mariana Pulido Parra <est.lauram.pulido@unimilitar.edu.co>
2026-04-05 13:21:55 -0500

    Se usó el metodo agregarPlato addLast para que el plato ingrese al final

--- src/main/java/com/mycompany/restaurante/ListaMenu.java
+++ src/main/java/com/mycompany/restaurante/ListaMenu.java
@@ -3,6 +3,9 @@ package com.mycompany.restaurante;
 
 import com.murcia.utils.ListaEnlazada;
 
- class ListaMenu extends ListaEnlazada<Plato> {
-    
+ class ListaMenu extends ListaEnlazada<Plato> { //clase que representa el menu heredado
+     
+    public void agregarPlato(Plato p){ //metodo para agregar plato al menu
+        addLast(p); //usar metodo de la clase ListaEnlazada heredada para gregar al final
+    }
 }
