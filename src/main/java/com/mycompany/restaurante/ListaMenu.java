5ad997240a6606549ba44b06df4ab5ae89e5cf13
Laura Mariana Pulido Parra <est.lauram.pulido@unimilitar.edu.co>
2026-04-05 13:59:18 -0500

    se creo el metodo mostarMenu en donde se hace un recorrido de la lista e imprime los datos. Hay que revisar error nodo

--- src/main/java/com/mycompany/restaurante/ListaMenu.java
+++ src/main/java/com/mycompany/restaurante/ListaMenu.java
@@ -8,4 +8,13 @@ import com.murcia.utils.ListaEnlazada;
     public void agregarPlato(Plato p){ //metodo para agregar plato al menu
         addLast(p); //usar metodo de la clase ListaEnlazada heredada para gregar al final
     }
+    public void mostrarMenu(){ 
+        Nodo<Plato>actual=head; //empieza desde el primero
+        
+        while(actual!=null){ //para recorrer la lista
+            Plato p= actual.get();
+            System.out.println(p.mostrar()); //imprimir el dato usando mostrar
+            actual=actual.get();
+        }
+    }
 }
