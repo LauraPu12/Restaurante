70133f14b5ff4944c9adb0bea2be63fe4511cb18
Laura Mariana Pulido Parra <est.lauram.pulido@unimilitar.edu.co>
2026-04-07 19:38:57 -0500

    La clase PilaHistorial no se hereda de un paquete de la libreria, por eso se creo el metodo push para agregar un pedido al historial

--- src/main/java/com/mycompany/restaurante/PilaHistorial.java
+++ src/main/java/com/mycompany/restaurante/PilaHistorial.java
@@ -9,4 +9,9 @@ public class PilaHistorial {
     public PilaHistorial(){
         cima = null; //constructor vacio
     }
+    public void push(Pedido p){ //metodo para agregar pedido al historial
+        Nodo<Pedido> nd = new Nodo(p); //nuevo nodo con el pedido
+        nd.setNext(cima); //el nuevo nodo apunta al anterior
+        cima = nd;//el nuevo es la cima
+    }
 }
