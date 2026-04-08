276de523c73b94f828c326b2c2f743bc4b6906a3
Laura Mariana Pulido Parra <est.lauram.pulido@unimilitar.edu.co>
2026-04-07 19:49:18 -0500

    en caso de que la pila no este vacia, se guarda el primer dato para luego moverlo y eliminarlo

--- src/main/java/com/mycompany/restaurante/PilaHistorial.java
+++ src/main/java/com/mycompany/restaurante/PilaHistorial.java
@@ -4,19 +4,24 @@ package com.mycompany.restaurante;
 import com.murcia.utils.Nodo;
 
 public class PilaHistorial {
-    Nodo<Pedido> cima; //nodo de encima de la pila
+    Nodo<Pedido> primero; //nodo de encima de la pila
     
     public PilaHistorial(){
-        cima = null; //constructor vacio
+        primero= null; //constructor vacio
     }
     public void push(Pedido p){ //metodo para agregar pedido al historial
         Nodo<Pedido> nd = new Nodo(p); //nuevo nodo con el pedido
-        nd.setNext(cima); //el nuevo nodo apunta al anterior
-        cima = nd;//el nuevo es la cima
+        nd.setNext(primero); //el nuevo nodo apunta al anterior
+        primero= nd;//el nuevo es la cima
     }
     public Pedido pop(){ //mrotdo para sacar el ultimo pedido
-        if (cima ==null){ //si esta vacio
+        if (primero ==null){ //si esta vacio
             return null; //no saca nada
+        }else{
+            Pedido p = primero.getData(); //guarda el dato de la cima
+            primero = primero.getNext(); //mueve el primero al siguiente nodo
+            return p;//devuelve el pedido que se elimino
         }
+        
     }
 }
