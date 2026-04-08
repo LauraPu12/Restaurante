f1ea479b3bc39257d655fcdf561bef5cd68250ea
Laura Mariana Pulido Parra <est.lauram.pulido@unimilitar.edu.co>
2026-04-07 19:28:24 -0500

    En la clase PilaHistorial se importó de la libreria el Nodo para crear el constructor vacio e inicializar la pila vacia

--- src/main/java/com/mycompany/restaurante/PilaHistorial.java
+++ src/main/java/com/mycompany/restaurante/PilaHistorial.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.restaurante;
 
-/**
- *
- * @author anair
- */
+import com.murcia.utils.Nodo;
+
 public class PilaHistorial {
+    Nodo<Pedido> cima; //nodo de encima de la pila
     
+    public PilaHistorial(){
+        cima = null; //constructor vacio
+    }
 }
