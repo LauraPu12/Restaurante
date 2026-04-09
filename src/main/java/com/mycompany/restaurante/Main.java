be6bb09601ac2afe12e1bfdc761ea7c222d22d70
Laura Mariana Pulido Parra <est.lauram.pulido@unimilitar.edu.co>
2026-04-09 08:58:39 -0500

    Se creo el menu de las opciones, sin embargo hay error ya que no encuentra add

--- src/main/java/com/mycompany/restaurante/Main.java
+++ src/main/java/com/mycompany/restaurante/Main.java
@@ -3,67 +3,23 @@ package com.mycompany.restaurante;
 import com.murcia.utils.Menu; // importa la clase Menu de la libreria
 import com.murcia.utils.Input; // importa clase para leer datos
 
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // objeto para leer teclado
         Restaurante r = new Restaurante(); // crea el restaurante
-        
-         int opcion; // variable para guardar la opción
+        // crea el menu usando la libreria
+        Menu menu = new Menu("MENU RESTAURANTE"); 
 
-        do{
-            System.out.println("\n--- MENU RESTAURANTE ---");
-            System.out.println("1. Agregar plato");
-            System.out.println("2. Mostrar menu");
-            System.out.println("3. Hacer pedido");
-            System.out.println("4. Atender pedido");
-            System.out.println("5. Mostrar pedidos");
-            System.out.println("6. Mostrar historial");
-            System.out.println("0. Salir");
+        menu.add("Agregar plato"); // opcion 1
+        menu.add("Mostrar menu"); // opcion 2
+        menu.add("Hacer pedido"); // opcion 3
+        menu.add("Atender pedido"); // opcion 4
+        menu.add("Mostrar pedidos"); // opcion 5
+        menu.add("Mostrar historial"); // opcion 6
+        menu.add("Salir"); // opcion 7
 
-            opcion = sc.nextInt(); // lee la opción
-           
-            sc.nextLine(); // limpia
-            
-            switch(opcion){
-                
-                case 1:
-                    
-                    System.out.println("-----------------------------");
-                    System.out.println("Nombre del plato:");
-                    String nombre = sc.nextLine(); // lee nombre
-                    System.out.println("Precio:");
-                    double precio = sc.nextDouble(); // lee precio
-                    r.agregarPlato(new Plato(nombre,precio)); // agrega plato
-                break;
-                case 2:
-                    r.mostrarMenu(); // muestra menú
-                break;
-                case 3:
-                    System.out.println("-----------------------------");
-                    System.out.println("Nombre del cliente:");
-                    String cliente = sc.nextLine(); // lee cliente
+        int opcion; // variable opcion
 
-                    System.out.println("Plato:");
-                    String plato = sc.nextLine(); // lee plato
-
-                    r.hacerPedido(new Pedido(cliente,plato)); // crea pedido
-                break;
-                case 4:
-                    r.atenderPedido(); // atiende el pedido
-                break;
-                case 5:
-                    r.mostrarPedidos(); // muestra pedidos en cola
-                break;
-                case 6:
-                    r.mostrarHistorial(); // muestra historial
-                break;
-
-            }
-
-        }while(opcion != 0); // repite hasta que el usuario salga
-        
         }
    
 }
