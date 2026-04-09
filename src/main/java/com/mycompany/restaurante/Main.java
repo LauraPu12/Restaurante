fcd25d321989d1a8db1805d7acb7f12c40f6ef67
Olga Lucia Guacaneme Archila <est.olga.guacaneme@unimilitar.edu.co>
2026-04-08 23:51:16 -0500

    Se escribe la variable de opcion y se inicializa el do while para organizar el menu que tiene el restaurante

--- src/main/java/com/mycompany/restaurante/Main.java
+++ src/main/java/com/mycompany/restaurante/Main.java
@@ -9,5 +9,23 @@ public class Main {
         Scanner sc = new Scanner(System.in); // objeto para leer teclado
         Restaurante r = new Restaurante(); // crea el restaurante
         
-    }
-}
+         int opcion; // variable para guardar la opción
+
+        do{
+            System.out.println("\n--- MENU RESTAURANTE ---");
+            System.out.println("1. Agregar plato");
+            System.out.println("2. Mostrar menu");
+            System.out.println("3. Hacer pedido");
+            System.out.println("4. Atender pedido");
+            System.out.println("5. Mostrar pedidos");
+            System.out.println("6. Mostrar historial");
+            System.out.println("0. Salir");
+
+            opcion = sc.nextInt(); // lee la opción
+            sc.nextLine(); // limpia buffer
+
+    
+
+        }
+        }
+        }
