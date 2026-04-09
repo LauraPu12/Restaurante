6937c62ecdf7d737f5b591e12bf355893a075146
Laura Mariana Pulido Parra <est.lauram.pulido@unimilitar.edu.co>
2026-04-09 09:23:10 -0500

    se corrije el case 1 para agregar plato

--- src/main/java/com/mycompany/restaurante/Main.java
+++ src/main/java/com/mycompany/restaurante/Main.java
@@ -20,5 +20,20 @@ public class Main {
             "6. Mostrar historial",
             "7. Salir"
         };
+         // crea el menú usando el constructor correcto
+        Menu menu = new Menu(opciones, '1', "-", "MENU RESTAURANTE");
+        char opcion; // variable 
+
+        do {
+            opcion = menu.select("Seleccione una opción: "); // muestra menú y lee opción
+
+            switch(opcion){
+
+                case '1': // agregar plato
+                    String nombre = Input.nextLine("Nombre del plato: "); // leer nombre
+                    double precio = Input.nextInt("Precio: ", 0, 1000000); // leer precio
+                    r.agregarPlato(new Plato(nombre, precio)); // agregar plato
+                break;
     }
 }
+        
