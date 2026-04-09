c0df02dcdab34f1fdc81b14f689e3b010504dd40
Laura Mariana Pulido Parra <est.lauram.pulido@unimilitar.edu.co>
2026-04-09 09:25:55 -0500

    se corrije el case 2 para mostrar el menu y case 3 para hacer el pedido

--- src/main/java/com/mycompany/restaurante/Main.java
+++ src/main/java/com/mycompany/restaurante/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         Restaurante r = new Restaurante(); // crea el restaurante
 
-        // arreglo con las opciones del menú
+        //opciones del menú
         String[] opciones = {
             "1. Agregar plato",
             "2. Mostrar menu",
@@ -20,19 +20,27 @@ public class Main {
             "6. Mostrar historial",
             "7. Salir"
         };
-         // crea el menú usando el constructor correcto
+         // crea el menú usando el constructor
         Menu menu = new Menu(opciones, '1', "-", "MENU RESTAURANTE");
         char opcion; // variable 
 
         do {
             opcion = menu.select("Seleccione una opción: "); // muestra menú y lee opción
-
             switch(opcion){
 
                 case '1': // agregar plato
                     String nombre = Input.nextLine("Nombre del plato: "); // leer nombre
                     double precio = Input.nextInt("Precio: ", 0, 1000000); // leer precio
                     r.agregarPlato(new Plato(nombre, precio)); // agregar plato
+                break;
+                 case '2': // mostrar menú
+                    r.mostrarMenu(); // imprime menú
+                break;
+
+                case '3': // hacer pedido
+                    String cliente = Input.nextLine("Nombre cliente: "); // leer cliente
+                    String plato = Input.nextLine("Plato: "); // leer plato
+                    r.hacerPedido(new Pedido(cliente, plato)); // crear pedido
                 break;
     }
 }
