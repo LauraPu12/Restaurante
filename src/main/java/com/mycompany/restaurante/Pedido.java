01278f6c26b8bd875800b9781313821f2930d4ed
Olga Lucia Guacaneme Archila <est.olga.guacaneme@unimilitar.edu.co>
2026-04-15 19:44:22 -0500

    Semodifico la clase pedido para que tubiera el id del plato

--- src/main/java/com/mycompany/restaurante/Pedido.java
+++ src/main/java/com/mycompany/restaurante/Pedido.java
@@ -3,6 +3,7 @@ package com.mycompany.restaurante;
 
 public class Pedido {
     
+    int id; // identificador unico del pedido
     String cliente; // Nombre del cliente
     String plato; // Nombre del plato pedido
 
@@ -11,12 +12,18 @@ public class Pedido {
     }
 
     // Constructor con parámetros
-    public Pedido(String cliente, String plato) {
+    public Pedido(int id, String cliente, String plato) {
+        this.id = id; // Asigna el id recibido
         this.cliente = cliente; // Asigna el cliente recibido
         this.plato = plato; // Asigna el plato recibido
     }
+      // Metodo para obtener el id
+    public int getId() {
+    return id; // Retorna el id
+    }
+    
     // Método para mostrar el pedido
     public String mostrar() {
-        return cliente + " pidio " + plato; // Retorna descripción del pedido
+        return "ID: " + id + " - " + cliente + " pidio " + plato; // Retorna descripción del pedido
     }
 }
